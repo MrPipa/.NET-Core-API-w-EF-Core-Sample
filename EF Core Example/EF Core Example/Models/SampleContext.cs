ebd0124d65cb199a9644f70970977a66149c7f82
Olli Arokari <olli.arokari@abo.fi>
2018-10-15 17:13:32 +0300

    Database context added

---
+++ EF Core Example/EF Core Example/Models/SampleContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core_Example.Models
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext(DbContextOptions<SampleContext> options)
+            : base(options)
+        { }
+
+        public DbSet<SampleModel> SampleModels { get; set; }
+    }
+}
