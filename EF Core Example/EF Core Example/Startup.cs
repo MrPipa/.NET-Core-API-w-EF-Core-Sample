5b3f271087f4360eeca74f397b222a98ea39ed8a
Olli Arokari <olli.arokari@abo.fi>
2018-10-15 17:14:20 +0300

    Project added

---
+++ EF Core Example/EF Core Example/Startup.cs
@@ -0,0 +1,47 @@
+﻿using EF_Core_Example.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF_Core_Example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Using local database, created if it doesn't exist
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=SampleDB;Trusted_Connection=True;";
+            services.AddDbContext<SampleContext>(options =>
+                options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
