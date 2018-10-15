b83c8fc186e8ff3a09f7c286781a4c826972b126
Olli Arokari <olli.arokari@abo.fi>
2018-10-15 17:13:45 +0300

    Sample model added

---
+++ EF Core Example/EF Core Example/Models/SampleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Core_Example.Models
+{
+    public class SampleModel
+    {
+        public Guid Id { get; set; }
+
+        public string SampleString { get; set; }
+        public int SampleInt { get; set; }
+    }
+}
